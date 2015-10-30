c20dc8d58fccf2c900d0859d2cc8828fb2d39304
Tino de Bruijn <tinodb@gmail.com>
2015-10-30 15:26:04 +0700

    REQUEST is deprecated, use GET instead

--- linaro_django_pagination/middleware.py
+++ linaro_django_pagination/middleware.py
@@ -35,7 +35,7 @@ def get_page(self, suffix):
     integer representing the current page.
     """
     try:
-        return int(self.REQUEST['page%s' % suffix])
+        return int(self.GET['page%s' % suffix])
     except (KeyError, ValueError, TypeError):
         return 1
 
